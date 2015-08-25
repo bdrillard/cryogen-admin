25385f86aacad8fae2a907317661dc2068878f5d
aleksanderesk <aleksanderesk@gmail.com>
2015-08-25 18:13:00 -0500

    updated project, gitignore

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject cryogen-admin "0.1.0-SNAPSHOT"
   :description "A tiny Compojure admin wrapper around Cryogen"
-  :url "https://github.com/bdrillard"
+  :url "https://github.com/bdrillard/cryogen-admin"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
@@ -11,4 +11,4 @@
                  [org.clojure/java.jdbc "0.4.1"]
                  [org.xerial/sqlite-jdbc "3.8.10.2"]]
   :scm {:name "git"
-        :url "https://github.com/bdrillard"})
+        :url "https://github.com/bdrillard/cryogen-admin"})
